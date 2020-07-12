77a02ac8ecee352d5a8089f4dd78a7187e86e62b
chenws <chenws@chenws-computer.lan>
2020-07-13 01:32:07 +0800

    快速操作execl，支持jdk1.6

---
+++ poi/src/main/java/core/api/ExportExcelController.java
@@ -0,0 +1,39 @@
+package core.api;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.collections4.map.HashedMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import core.util.EasyPOIUtil;
+@RestController
+@RequestMapping("/ExportExcelController")
+public class ExportExcelController {
+	
+	@GetMapping("/exportExcel")
+	public void exportExcel (HttpServletResponse response) throws Exception {
+		List<Map<String, Object>> list = new ArrayList<Map<String,Object>>();
+		for (int i = 0; i < 10; i++) {
+		    Map<String, Object> map = new HashedMap<String, Object>();
+			map.put("NAME", "张三四大佛山的哈佛山大佛"+i);
+			map.put("AGE", 26+i);
+			map.put("GRADE", "12312"+i);
+			list.add(map);
+		}
+		String[] eName = {"NAME","AGE","GRADE"};
+		String[] cName = {"姓名","年龄","成绩"};
+		EasyPOIUtil.exportExcel(response, list, eName, cName, "无创建对象");
+	}
+	
+
+	
+
+}
---
+++ poi/src/main/java/core/util/EasyPOIUtil.java
@@ -0,0 +1,99 @@
+package core.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.collections.CollectionUtils;
+
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.metadata.Sheet;
+import com.alibaba.excel.metadata.Table;
+import com.alibaba.excel.support.ExcelTypeEnum;
+
+/**
+ * 
+ * @author chenws
+ *    支持jdk1.6
+ */
+public class EasyPOIUtil {
+	
+
+    public static void exportExcel(HttpServletResponse response,List<Map<String, Object>> list, String[] eName, String[] cName, String fileName) throws Exception {
+ 
+        ServletOutputStream out = null;
+        ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX);
+        try {
+            out = response.getOutputStream();
+          
+ 
+            // 设置EXCEL名称
+           // String fileName = new String(("SystemExcel").getBytes(), "UTF-8");
+ 
+            // 设置SHEET名称
+            Sheet sheet = new Sheet(1, 0);
+            sheet.setSheetName("系统列表sheet1");
+ 
+            // 设置标题
+            Table table = new Table(1);
+			List<List<String>> headList = new ArrayList<List<String>>();
+        	for (String name : cName) {
+				List<String> head = new ArrayList<String>();
+				head.add(name);
+				headList.add(head);
+			}
+        	   table.setHead(headList);
+            // 查数据写EXCEL
+            List<List<Object>> dataList = new ArrayList<List<Object>>();
+            
+            if (!CollectionUtils.isEmpty(list)) {
+            	
+            	for (Map<String, Object> map : list) {
+    				List<Object> data = new ArrayList<Object>();
+    				for (String name : eName) {
+    					data.add(map.get(name));
+    				}
+    				dataList.add(data);
+    			}
+               
+            }
+            writer.write1(dataList, sheet, table);
+ 
+            // 下载EXCEL
+            response.setHeader("Content-Disposition", "attachment;filename=" + new String((fileName).getBytes("gb2312"), "ISO-8859-1") + ".xls");
+            response.setContentType("multipart/form-data");
+            response.setCharacterEncoding("utf-8");
+          
+            out.flush();
+ 
+        }catch (Exception e) {
+        	response.reset();
+			response.setContentType("application/json");
+			response.setCharacterEncoding("utf-8");
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("status", "failure");
+			map.put("message", "下载文件失败" + e.getMessage());
+			// response.getWriter().println(JSON.toJSONString(map)); // fastjson，不引入
+			response.getWriter().println(map);
+		} finally {
+			if (writer !=null ) {
+				writer.finish();
+			}
+        	
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+ 
+    }
+
+}
